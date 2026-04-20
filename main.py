2c7f082819a9ddff6b5f5034105cef3df3cf92d2
KamiraKirai <levan20092004@gmail.com>
2026-04-20 08:53:18 +0700

    test the trigger of github action

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def hello():
-    return "Hello!!! from GitHub Actions Demo!"
+    return "Hello!!! from GitHub Actions Demo!, This is a test for CI/CD pipeline using GitHub Actions."
 
 if __name__ == "__main__":
     print(hello())
